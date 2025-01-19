cda1f4aee2bca31ebc56dfd6cae184adfc5598a7
Phillip Potter <phil@philpotter.co.uk>
2025-01-19 23:06:31 +0000

    Remove as_i32 function, was stupid idea. Also, implement logical_rshift as in the original C version using a combination of a trait and type aliases.

--- philpsx_utility/src/lib.rs
+++ philpsx_utility/src/lib.rs
@@ -3,12 +3,112 @@
 
 // This crate contains useful utility functions that can be used throughout the codebase.
 
-/// This function provides a shorthand way of casting 0x hexadecimal-style literals to
-/// i32, so we don't have to keep writing `u32 as i32` on the end, which feels weird
-/// given we are just representing the fact that these values are storable inside an
-/// i32 (as in the original C source). It's debatable whether this is any cleaner than
-/// just writing `0xFFFFFFFFu32 as i32`, but it feels nicer to me.
-#[inline(always)]
-pub fn as_i32(num: u32) -> i32 {
-    num as i32
+/// Exists to allow us to define custom trait operations on `i32`.
+type CustomInt32 = i32;
+
+/// Exists to allow us to define custom trait operations on `i64`.
+type CustomInt64 = i64;
+
+/// Exists to allow us to define custom trait operations on `u32`.
+type CustomUInt32 = u32;
+
+/// Exists to allow us to define custom trait operations on `u64`.
+type CustomUInt64 = u64;
+
+/// This trait exists to allow us to implement `logical_rshift` in the same way as the
+/// C macro original, at least from a semantic perspective.
+pub trait CustomInteger {
+
+    type Output;
+
+    /// This function should return a signed value, logically right-shifted by the
+    /// specified amount and of the same width as the original, whether it is signed
+    /// or unsigned.
+    fn logical_rshift(self, shift_by: i32) -> Self::Output;
+}
+
+impl CustomInteger for CustomInt32 {
+
+    type Output = i32;
+
+    /// Logically shifts right by specified amount, returning `i32`.
+    fn logical_rshift(self, shift_by: i32) -> Self::Output {
+        ((self as u32) >> shift_by) as Self::Output
+    }
+}
+
+impl CustomInteger for CustomInt64 {
+
+    type Output = i64;
+
+    /// Logically shifts right by specified amount, returning `i64`.
+    fn logical_rshift(self, shift_by: i32) -> Self::Output {
+        ((self as u64) >> shift_by) as Self::Output
+    }
+}
+
+impl CustomInteger for CustomUInt32 {
+
+    type Output = i32;
+
+    /// Logically shifts right by specified amount, returning `i32`.
+    fn logical_rshift(self, shift_by: i32) -> Self::Output {
+        (self >> shift_by) as Self::Output
+    }
+}
+
+impl CustomInteger for CustomUInt64 {
+
+    type Output = i64;
+
+    /// Logically shifts right by specified amount, returning `i64`.
+    fn logical_rshift(self, shift_by: i32) -> Self::Output {
+        (self >> shift_by) as Self::Output
+    }
+}
+
+/// Re-exported stdlib `min` function, to keep all our utility functions together
+/// here in the same way they are for the C macro versions.
+pub use std::cmp::min;
+
+#[cfg(test)]
+mod tests {
+
+    use super::CustomInteger;
+
+    #[test]
+    fn logical_rshift_should_work_as_expected_for_i32() {
+
+        let input = 0xFFFFFFFFu32 as i32;
+        let output = input.logical_rshift(1);
+
+        assert_eq!(output, 0x7FFFFFFF);
+    }
+
+    #[test]
+    fn logical_rshift_should_work_as_expected_for_i64() {
+
+        let input = 0xFFFFFFFFFFFFFFFFu64 as i64;
+        let output = input.logical_rshift(1);
+
+        assert_eq!(output, 0x7FFFFFFFFFFFFFFF);
+    }
+
+    #[test]
+    fn logical_rshift_should_work_as_expected_for_u32() {
+
+        let input = 0xFFFFFFFFu32;
+        let output = input.logical_rshift(1);
+
+        assert_eq!(output, 0x7FFFFFFF);
+    }
+
+    #[test]
+    fn logical_rshift_should_work_as_expected_for_u64() {
+
+        let input = 0xFFFFFFFFFFFFFFFFu64;
+        let output = input.logical_rshift(1);
+
+        assert_eq!(output, 0x7FFFFFFFFFFFFFFF);
+    }
 }
