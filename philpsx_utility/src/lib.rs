605e55770109822cfdbdabef613c57b08e79acbf
Phillip Potter <phil@philpotter.co.uk>
2025-03-21 21:06:05 +0000

    Remove CustomInteger implementations for u32 and u64. Analysis of the original C codebase shows I never even used the equivalent macro variants there.

--- philpsx_utility/src/lib.rs
+++ philpsx_utility/src/lib.rs
@@ -9,12 +9,6 @@ type CustomInt32 = i32;
 /// Exists to allow us to define custom trait operations on `i64`.
 type CustomInt64 = i64;
 
-/// Exists to allow us to define custom trait operations on `u32`.
-type CustomUInt32 = u32;
-
-/// Exists to allow us to define custom trait operations on `u64`.
-type CustomUInt64 = u64;
-
 /// This trait exists to allow us to implement `logical_rshift` in the same way as the
 /// C macro original, at least from a semantic perspective.
 pub trait CustomInteger {
@@ -22,8 +16,7 @@ pub trait CustomInteger {
     type Output;
 
     /// This function should return a signed value, logically right-shifted by the
-    /// specified amount and of the same width as the original, whether it is signed
-    /// or unsigned.
+    /// specified amount and of the same width as the original, without sign-extension.
     fn logical_rshift(self, shift_by: i32) -> Self::Output;
 }
 
@@ -46,28 +39,6 @@ impl CustomInteger for CustomInt64 {
     #[inline(always)]
     fn logical_rshift(self, shift_by: i32) -> Self::Output {
         ((self as u64) >> shift_by) as Self::Output
-    }
-}
-
-impl CustomInteger for CustomUInt32 {
-
-    type Output = i32;
-
-    /// Logically shifts right by specified amount, returning `i32`.
-    #[inline(always)]
-    fn logical_rshift(self, shift_by: i32) -> Self::Output {
-        (self >> shift_by) as Self::Output
-    }
-}
-
-impl CustomInteger for CustomUInt64 {
-
-    type Output = i64;
-
-    /// Logically shifts right by specified amount, returning `i64`.
-    #[inline(always)]
-    fn logical_rshift(self, shift_by: i32) -> Self::Output {
-        (self >> shift_by) as Self::Output
     }
 }
 
@@ -110,24 +81,6 @@ mod tests {
     fn logical_rshift_should_work_as_expected_for_i64() {
 
         let input = 0xFFFFFFFFFFFFFFFFu64 as i64;
-        let output = input.logical_rshift(1);
-
-        assert_eq!(output, 0x7FFFFFFFFFFFFFFF);
-    }
-
-    #[test]
-    fn logical_rshift_should_work_as_expected_for_u32() {
-
-        let input = 0xFFFFFFFFu32;
-        let output = input.logical_rshift(1);
-
-        assert_eq!(output, 0x7FFFFFFF);
-    }
-
-    #[test]
-    fn logical_rshift_should_work_as_expected_for_u64() {
-
-        let input = 0xFFFFFFFFFFFFFFFFu64;
         let output = input.logical_rshift(1);
 
         assert_eq!(output, 0x7FFFFFFFFFFFFFFF);
