1ccad12bf1ad4f3ece4fa99c946c1822f124b3be
Phillip Potter <phil@philpotter.co.uk>
2025-09-25 21:03:12 +0100

    Port over instruction cache and associated logic.

--- philpsx_core/src/cpu.rs
+++ philpsx_core/src/cpu.rs
@@ -18,8 +18,8 @@ pub trait Cpu {
     /// Implementations must use this to set the system bus holder.
     fn set_system_bus_holder(
         &mut self,
-        holder: SystemBusHolder,
-        bridge: &mut dyn CpuBridge
+        bridge: &mut dyn CpuBridge,
+        holder: SystemBusHolder
     );
 
     /// Implementations must use this to retrieve the system bus holder.
@@ -32,4 +32,34 @@ pub trait Cpu {
 /// This trait provides an implementation-opaque way of the CPU
 /// calling methods from elsewhere in the system via a 'bridge'.
 pub trait CpuBridge {
+
+    /// The CPU must call this to append a cycle count to the system count.
+    fn append_sync_cycles(&mut self, cpu: &mut dyn Cpu, cycles: i32);
+
+    /// The CPU must call this to determine how many stall cycles are needed.
+    fn how_how_many_stall_cycles(&self, cpu: &mut dyn Cpu) -> i32;
+
+    /// The CPU must call this to determine if an address should be incremented.
+    fn ok_to_increment(&self, cpu: &mut dyn Cpu) -> bool;
+
+    /// The CPU must call this to determine if the scratchpad is enabled.
+    fn scratchpad_enabled(&self, cpu: &mut dyn Cpu) -> bool;
+
+    /// The CPU must call this to determine if the instruction cache is enabled.
+    fn instruction_cache_enabled(&self, cpu: &mut dyn Cpu) -> bool;
+
+    /// The CPU must call this to read a byte from the system bus.
+    fn read_byte(&self, cpu: &mut dyn Cpu, address: i32) -> i8;
+
+    /// The CPU must call this to read a word from the system bus.
+    fn read_word(&self, cpu: &mut dyn Cpu, address: i32) -> i32;
+
+    /// The CPU must call this to write a byte to the system bus.
+    fn write_byte(&mut self, cpu: &mut dyn Cpu, address: i32, value: i8);
+
+    /// The CPU must call this to write a word to the system bus.
+    fn write_word(&mut self, cpu: &mut dyn Cpu, address: i32, value: i8);
+
+    /// The CPU must call this to increment all interrupt-relevant counters.
+    fn increment_interrupt_counters(&mut self, cpu: &mut dyn Cpu);
 }
--- philpsx_core/src/cpu/r3051.rs
+++ philpsx_core/src/cpu/r3051.rs
@@ -2,7 +2,7 @@
 // r3051.rs - Copyright Phillip Potter, 2025, under GPLv3 only.
 
 use super::{Cpu, CpuBridge};
-use philpsx_utility::SystemBusHolder;
+use philpsx_utility::{CustomInteger, SystemBusHolder};
 use mips_exception::MIPSException;
 use cp0::CP0;
 use cp2::CP2;
@@ -21,6 +21,15 @@ mod cp2;
 
 /// The number of general CPU registers.
 const REGISTER_COUNT: usize = 32;
+
+/// The maximum size of the instruction cache data array in entries.
+const INSTRUCTION_CACHE_DATA_MAX_ENTRIES: usize = 4096;
+
+/// The maximum size of the instruction cache tag array in entries.
+const INSTRUCTION_CACHE_TAG_MAX_ENTRIES: usize = 256;
+
+/// The maximum size of the instruction cache validity array in entries.
+const INSTRUCTION_CACHE_VALID_MAX_ENTRIES: usize = 256;
 
 /// This structure represents the internal state of the R3051 processor.
 /// It contains registers, and internal subcomponents.
@@ -54,6 +63,12 @@ pub struct R3051 {
     cycles: i32,
     gte_cycles: i32,
     total_cycles: i64,
+
+    // Instruction cache variables - previously these were modelled
+    // separately in the C version. All are heap allocated.
+    instruction_cache_data: Vec<i8>,
+    instruction_cache_tag: Vec<i32>,
+    instruction_cache_valid: Vec<bool>,
 }
 
 /// Implementation functions for the R3051 component itself.
@@ -92,6 +107,11 @@ impl R3051 {
             cycles: 0,
             gte_cycles: 0,
             total_cycles: 0,
+
+            // Setup instruction cache variables.
+            instruction_cache_data: vec![0; INSTRUCTION_CACHE_DATA_MAX_ENTRIES],
+            instruction_cache_tag: vec![0; INSTRUCTION_CACHE_TAG_MAX_ENTRIES],
+            instruction_cache_valid: vec![false; INSTRUCTION_CACHE_VALID_MAX_ENTRIES],
         };
 
         r3051.reset();
@@ -105,6 +125,99 @@ impl R3051 {
         // Patch in later with proper reset exception vector.
         self.program_counter = self.sccp.get_reset_exception_vector();
     }
+
+    /// This function checks for an instruction cache hit. The address provided
+    /// must be physical and not virtual.
+    fn check_for_instruction_cache_hit(&self, address: i32) -> bool {
+
+        let tag_index = (address.logical_rshift(4) & 0xFF) as usize;
+        let expected_tag = address.logical_rshift(12) & 0xFFFFF;
+
+        self.instruction_cache_tag[tag_index] == expected_tag &&
+        self.instruction_cache_valid[tag_index]
+    }
+
+    /// This function retrieves a word from the correct address.
+    fn read_instruction_cache_word(&self, address: i32) -> i32 {
+
+        let data_index = (address & 0xFFC) as usize;
+
+        (((self.instruction_cache_data[data_index] as i32) & 0xFF) << 24) |
+        (((self.instruction_cache_data[data_index + 1] as i32) & 0xFF) << 16) |
+        (((self.instruction_cache_data[data_index + 2] as i32) & 0xFF) << 8) |
+        ((self.instruction_cache_data[data_index + 3] as i32) & 0xFF)
+    }
+
+    /// This function retrieves a byte from the correct address.
+    fn read_instruction_cache_byte(&self, address: i32) -> i8 {
+
+        let data_index = (address & 0xFFF) as usize;
+
+        self.instruction_cache_data[data_index]
+    }
+
+    /// This function writes a word to the correct address, and invalidates
+    /// the correct cache line.
+    fn write_instruction_cache_word(&mut self, address: i32, value: i32) {
+
+        let data_index = (address & 0xFFC) as usize;
+
+        // Update correct word.
+        self.instruction_cache_data[data_index] = value.logical_rshift(24) as i8;
+        self.instruction_cache_data[data_index + 1] = value.logical_rshift(16) as i8;
+        self.instruction_cache_data[data_index + 2] = value.logical_rshift(8) as i8;
+        self.instruction_cache_data[data_index + 3] = value as i8;
+
+        // Invalidate line if cache is isolated.
+        if self.sccp.is_data_cache_isolated() {
+            let tag_index = (address.logical_rshift(4) & 0xFF) as usize;
+            let tag = address.logical_rshift(12) & 0xFFFFF;
+            self.instruction_cache_tag[tag_index] = tag;
+            self.instruction_cache_valid[tag_index] = false;
+        }
+    }
+
+    /// This function writes a byte to the correct address, and invalidates
+    /// the correct cache line.
+    fn write_instruction_cache_byte(&mut self, address: i32, value: i8) {
+
+        let data_index = (address & 0xFFF) as usize;
+
+        // Update correct byte.
+        self.instruction_cache_data[data_index] = value;
+
+        // Invalidate line if cache is isolated.
+        if self.sccp.is_data_cache_isolated() {
+            let tag_index = (address.logical_rshift(4) & 0xFF) as usize;
+            let tag = address.logical_rshift(12) & 0xFFFFF;
+            self.instruction_cache_tag[tag_index] = tag;
+            self.instruction_cache_valid[tag_index] = false;
+        }
+    }
+
+    /// This function refills a cache line using an address.
+    fn refill_instruction_cache_line(&mut self, bridge: &mut dyn CpuBridge, address: i32) {
+        // Refill a line - four words.
+        // Check if cache is isolated first.
+        if self.sccp.is_data_cache_isolated() {
+            return;
+        }
+
+        // Calculate tag index and tag.
+        let tag_index = (address.logical_rshift(4) & 0xFF) as usize;
+        let tag = address.logical_rshift(12) & 0xFFFFF;
+
+        // Write tag and valid flag.
+        self.instruction_cache_tag[tag_index] = tag;
+        self.instruction_cache_valid[tag_index] = true;
+
+        // Refill cache line.
+        let starting_address = (address & 0xFFFFFFF0_u32 as i32) as usize;
+        for offset in 0..16 {
+            self.instruction_cache_data[(starting_address + offset) & 0xFFF] =
+                bridge.read_byte(self, starting_address as i32);
+        }
+    }
 }
 
 /// Implementation functions to be called from anything that understands what
@@ -114,8 +227,8 @@ impl Cpu for R3051 {
     /// Set the system bus holder.
     fn set_system_bus_holder(
         &mut self,
-        holder: SystemBusHolder,
-        _bridge: &mut dyn CpuBridge
+        _bridge: &mut dyn CpuBridge,
+        holder: SystemBusHolder
     ) {
         self.system_bus_holder = holder;
     }
