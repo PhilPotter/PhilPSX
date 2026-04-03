a39ea0fe9f16d3719ae8a53cc3e43f2172cccb89
Phillip Potter <phil@philpotter.co.uk>
2026-04-03 21:48:45 +0100

    Track detection now working. Properly reads each track's pregap (if it has one) and all of a track's indexes. It also calculates the ends of each track index using what I think is the correct logic based on NOPSX docs. I decided to ignore my original C version completely here and just design a new version from first principles.
    Tomorrow I will write some tests for this code (and indeed restructure
    it a bit to make testing it easier).

--- philpsx_core/src/cdrom_drive/psx_cdrom_drive/psx_cd.rs
+++ philpsx_core/src/cdrom_drive/psx_cdrom_drive/psx_cd.rs
@@ -15,8 +15,12 @@ use std::{
 
 use philpsx_utility::error::PhilPSXError;
 
+/// This gap size represents the first initial two second gap before data begins.
+const INITIAL_GAP_SIZE: usize = 2352 * 150; // Two seconds worth (at 75 frames per second).
+
 /// This enum represents all possible track types.
 /// It is intentionally incomplete for now to keep things simpler.
+#[derive(Copy, Clone, Debug)]
 enum PsxCdTrackType {
     AUDIO,
     MODE2_2352,
@@ -25,19 +29,22 @@ enum PsxCdTrackType {
 /// This enum represents the possible states whilst detecting tracks
 /// from a cue file.
 enum PsxCdTrackDetectionState {
-    PRE_TRACK_LISTINGS,
-    ON_TRACK_LINE,
+    PreTrackListings,
+    OnTrackLine,
+    ReadingTrackIndexes,
+    CreatingTrack,
 }
 
 /// This struct models a CD itself, and abstracts image type away from the emulator,
 /// allowing different image types to be supported.
+#[derive(Debug)]
 pub struct PsxCd {
 
     // This stores the file reference for the CD image.
     cd_file: Option<BufReader<File>>,
 
     // This stores the size of the CD image in bytes.
-    cd_file_size: u64,
+    cd_file_size: usize,
 
     // This allows us to keep a list of tracks from the image.
     track_list: Vec<PsxCdTrack>,
@@ -47,14 +54,41 @@ pub struct PsxCd {
 /// mapping the entire file with mmap as this requires unsafe and I want to see how far I can
 /// get without it. For now, we simply use an optional buffered file descriptor and seek/read
 /// using it.
-pub struct PsxCdTrack {
+#[derive(Debug)]
+struct PsxCdTrack {
 
     // Track properties.
     track_number: i32,
     track_type: PsxCdTrackType,
-    track_start: i64,
-    track_end: i64,
-    offset: i64,
+    track_pregap: Option<PsxCdTrackPregap>, // Here as a marker more than anything else.
+    track_indexes: Vec<PsxCdTrackIndex>,
+    offset: usize,
+}
+
+/// This struct models a track's various sections that appear as INDEX in a cue file.
+#[derive(Debug)]
+struct PsxCdTrackIndex {
+
+    // Index properties - these are the first and last byte index
+    // within the CD, without offset applied.
+    track_start: usize,
+    track_end: usize,
+}
+
+/// This struct models a track's pregap (if it has one).
+#[derive(Copy, Clone, Debug)]
+struct PsxCdTrackPregap {
+
+    // Pregap properties.
+    pregap_size: usize,
+}
+
+/// This type lets us store intermediate tracks without final end values etc.
+struct IntermediateTrack {
+
+    track_type: PsxCdTrackType,
+    track_pregap: Option<PsxCdTrackPregap>,
+    track_indexes: Vec<usize>,
 }
 
 /// Implementation functions for PsxCd.
@@ -130,10 +164,15 @@ impl PsxCd {
         let mut bin_file_reader = get_bin_file_reader(&cue_file_lines, cue_file_path)?;
 
         // Get our BIN file size.
-        let bin_file_size = get_bin_file_size(&mut bin_file_reader)?;
+        let bin_size = get_bin_file_size(&mut bin_file_reader)?;
 
         // Now calculate our track listings properly.
-        let track_listings = get_track_listings(&cue_file_lines);
+        let track_list = get_track_listings(&cue_file_lines, bin_size)?;
+
+        // Set everything in our CD.
+        self.cd_file = Some(bin_file_reader);
+        self.cd_file_size = bin_size;
+        self.track_list = track_list;
 
         Ok(())
     }
@@ -238,9 +277,9 @@ fn get_bin_file_reader(cue_file_lines: &[String], cue_file_path: &Path) -> Resul
 }
 
 /// This function calculates the size of our BIN file.
-fn get_bin_file_size(bin_file_reader: &mut BufReader<File>) -> Result<u64, Box<dyn Error>> {
+fn get_bin_file_size(bin_file_reader: &mut BufReader<File>) -> Result<usize, Box<dyn Error>> {
 
-    Ok(bin_file_reader.seek(SeekFrom::End(0))?)
+    Ok(bin_file_reader.seek(SeekFrom::End(0))? as usize)
 }
 
 /// This function gets the track listings from the cue file. It doesn't
@@ -248,34 +287,189 @@ fn get_bin_file_size(bin_file_reader: &mut BufReader<File>) -> Result<u64, Box<d
 /// as this is what the original works with. It also doesn't support anything
 /// other than INDEX 01. PREGAP and INDEX 00 are not yet supported but will
 /// be in due course.
-fn get_track_listings(cue_file_lines: &[String]) -> Result<u64, Box<dyn Error>> {
+fn get_track_listings(cue_file_lines: &[String], bin_size: usize) -> Result<Vec<PsxCdTrack>, Box<dyn Error>> {
 
     // Variables here will track our state as we move through each line.
-    let mut current_state = PsxCdTrackDetectionState::PRE_TRACK_LISTINGS;
-    let current_track_offset = 2352 * 150; // Two seconds worth (at 75 frames per second).
+    let mut current_state = PsxCdTrackDetectionState::PreTrackListings;
+    let mut current_track_type: Option<PsxCdTrackType> = None;
+    let mut current_track_pregap: Option<PsxCdTrackPregap> = None;
+    let mut current_indexes: Vec<usize> = vec![];
+    let mut tracks: Vec<IntermediateTrack> = vec![];
+
+    // This lambda creates a track for us in the intermediate format.
+    // It allows us to not duplicate the code.
+    let create_intermediate_track = |
+        track_type: &Option<PsxCdTrackType>,
+        pregrap: &Option<PsxCdTrackPregap>,
+        indexes: &Vec<usize>
+    | -> Result<IntermediateTrack, Box<dyn Error>> {
+
+        // Unwrap the track type - it's an error if we didn't find it.
+        let track_type = match track_type {
+            Some(t) => Ok(*t),
+            None => Err(PhilPSXError::error("CD: No track type when creating track")),
+        }?;
+
+        // Copy the pregap.
+        let pregap = *pregrap;
+
+        // Copy the indexes.
+        if indexes.is_empty() {
+            return Err(PhilPSXError::error("CD: Track with no indexes encountered"))
+        }
+        let indexes = indexes.clone();
+
+        Ok(
+            IntermediateTrack {
+                track_type,
+                track_pregap: pregap,
+                track_indexes: indexes
+            }
+        )
+    };
     
     // Use a very basic state machine here to construct the tracks.
     let mut line_index = 0;
     while line_index < cue_file_lines.len() {
+
+        let current_line = &cue_file_lines[line_index];
+
         match current_state {
-            
+
             // We are pre-track listings here.
-            PsxCdTrackDetectionState::PRE_TRACK_LISTINGS => {
-                if cue_file_lines[line_index].starts_with("TRACK") {
-                    current_state = PsxCdTrackDetectionState::ON_TRACK_LINE;
+            PsxCdTrackDetectionState::PreTrackListings => {
+                if current_line.starts_with("TRACK") {
+                    current_state = PsxCdTrackDetectionState::OnTrackLine;
                 } else {
                     line_index += 1;
                 }
             },
 
             // We are now on a track line.
-            PsxCdTrackDetectionState::ON_TRACK_LINE => {
+            PsxCdTrackDetectionState::OnTrackLine => {
+                current_track_type = Some(get_track_type(current_line)?);
+                current_state = PsxCdTrackDetectionState::ReadingTrackIndexes;
+                line_index += 1;
+            },
 
+            // We are now reading an index from a track.
+            PsxCdTrackDetectionState::ReadingTrackIndexes => {
+
+                if current_line.starts_with("PREGAP") {
+                    // We found a pregap, which isn't part of the bin file.
+                    // Modify the offset accordingly.
+                    current_track_pregap = Some(
+                        PsxCdTrackPregap {
+                            pregap_size: get_pregap_size(current_line)?
+                        }
+                    );
+                    line_index += 1;
+                }
+                else if current_line.starts_with("INDEX") {
+                    // Read this index start into current vector.
+                    current_indexes.push(
+                        get_track_index_start(current_line)?
+                    );
+                    line_index += 1;
+                } else if current_line.starts_with("TRACK") {
+                    // We found the next track, mark us as done with this track.
+                    current_state = PsxCdTrackDetectionState::CreatingTrack;
+                } else {
+                    // For anything else we should keep going.
+                    line_index += 1;
+                }
+            },
+
+            // We are now creating a track.
+            PsxCdTrackDetectionState::CreatingTrack => {
+
+                let track = create_intermediate_track(
+                    &current_track_type,
+                    &current_track_pregap,
+                    &current_indexes
+                )?;
+
+                tracks.push(track);
+                current_track_type = None;
+                current_track_pregap = None;
+                current_indexes.clear();
+                current_state = PsxCdTrackDetectionState::OnTrackLine;
             },
         };
     }
 
-    Ok(0)
+    // Create the last track now.
+    tracks.push(
+        create_intermediate_track(
+            &current_track_type,
+            &current_track_pregap,
+            &current_indexes
+        )?
+    );
+
+    // Now we need to go through each track, adjust each MM:SS:FF byte address to the address
+    // it would be on a real CD, and store this alongside the offset. Also, we need to calculate
+    // end addresses of each index.
+    let mut current_offset = INITIAL_GAP_SIZE;
+    let final_tracks: Vec<PsxCdTrack> = tracks
+        .iter()
+        .enumerate()
+        .map(|i_and_track| {
+            let i = i_and_track.0;
+            let track = i_and_track.1;
+
+            // Adjust offset if a pregap is present.
+            current_offset += match track.track_pregap {
+                Some(track_pregap) => track_pregap.pregap_size,
+                None => 0,
+            };
+
+            // Now adjust the indexes into the form we need.
+            let track_indexes = track.track_indexes
+                .iter()
+                .enumerate()
+                .map(|j_and_index| {
+                    let j = j_and_index.0;
+                    let index = j_and_index.1;
+
+                    PsxCdTrackIndex {
+                        track_start: index + current_offset,
+                        track_end: if j == track.track_indexes.len() - 1 {
+
+                            // This is the last index, we either need to get the beginning of the next track - 1 byte
+                            // as our end position, or we need the BIN file size itself.
+                            if i == tracks.len() - 1 {
+
+                                // This is the last track, so use the BIN file size.
+                                bin_size + current_offset
+                            } else {
+
+                                // We can get the beginning of the next track and use this. The next
+                                // track might have a pregap, but it isn't part of the current offset (yet).
+                                // Therefore, applying the current offset to the beginning of the next
+                                // track's first index will in theory get us to the start address of
+                                // the pregap itself (which we will -1 from at the end of the outer if).
+                                let next_track = &tracks[i+1];
+                                next_track.track_indexes[0] + current_offset
+                            }
+                        } else {
+                            track.track_indexes[j+1] + current_offset
+                        } - 1 // Subtract 1 as this is the last valid index of the track, not its size.
+                    }
+                })
+                .collect();
+
+            PsxCdTrack {
+                track_number: (i as i32) + 1,
+                track_type: track.track_type,
+                track_pregap: track.track_pregap,
+                track_indexes,
+                offset: current_offset,
+            }
+        })
+        .collect();
+
+    Ok(final_tracks)
 }
 
 /// This function gets the track type of a TRACK line.
@@ -284,7 +478,7 @@ fn get_track_type(track_line: &String) -> Result<PsxCdTrackType, Box<dyn Error>>
     if !track_line.starts_with("TRACK") {
         return Err(
             PhilPSXError::error(
-                &format!("CD: Track type routine was supplied non TRACK line: {}", track_line)
+                &format!("CD: Track type routine was supplied a non-TRACK line: {}", track_line)
             )
         );
     }
@@ -298,9 +492,11 @@ fn get_track_type(track_line: &String) -> Result<PsxCdTrackType, Box<dyn Error>>
             )
         )?;
 
+    // For now, just handle MODE2/2352 and AUDIO types.
     match track_type_str {
-        
-        // Only care about Mode 2 2352 byte sectors for now.
+
+        "AUDIO" => Ok(PsxCdTrackType::AUDIO),
+
         "MODE2/2352" => Ok(PsxCdTrackType::MODE2_2352),
 
         _ => Err(
@@ -309,4 +505,79 @@ fn get_track_type(track_line: &String) -> Result<PsxCdTrackType, Box<dyn Error>>
             )
         )
     }
+}
+
+/// This function gets the start position from an index line
+fn get_track_index_start(index_line: &String) -> Result<usize, Box<dyn Error>> {
+
+    if !index_line.starts_with("INDEX") {
+        return Err(
+            PhilPSXError::error(
+                &format!("CD: Track index start routine was supplied a non-INDEX line: {}", index_line)
+            )
+        );
+    }
+
+    let index_time_str = index_line
+        .split_whitespace()
+        .last()
+        .ok_or(
+            PhilPSXError::error(
+                &format!("CD: Could not get track index start from line: {}", index_line)
+            )
+        )?;
+
+    get_bytes_from_mm_ss_ff(index_time_str)
+}
+
+/// This function gets the size from a PREGAP line.
+fn get_pregap_size(pregap_line: &String) -> Result<usize, Box<dyn Error>> {
+
+    if !pregap_line.starts_with("PREGAP") {
+        return Err(
+            PhilPSXError::error(
+                &format!("CD: Track pregap size routine was supplied a non-PREGAP line: {}", pregap_line)
+            )
+        );
+    }
+
+    let pregap_time_str = pregap_line
+        .split_whitespace()
+        .last()
+        .ok_or(
+            PhilPSXError::error(
+                &format!("CD: Could not get track index start from line: {}", pregap_line)
+            )
+        )?;
+
+    get_bytes_from_mm_ss_ff(pregap_time_str)
+}
+
+/// This converts the minutes:seconds:frames format into bytes.
+fn get_bytes_from_mm_ss_ff(time_str: &str) -> Result<usize, Box<dyn Error>> {
+
+    // Now split into minutes, seconds and frames.
+    let mins_secs_frames: Result<Vec<usize>, _> = time_str
+        .split(":")
+        .map(|value| value.parse::<usize>())
+        .collect();
+    let mins_secs_frames = mins_secs_frames?;
+
+    // If we don't have exactly three segments, then bail.
+    if mins_secs_frames.len() != 3 {
+        return Err(
+            PhilPSXError::error(
+                &format!("CD: Time string could not be split and parsed: {}", time_str)
+            )
+        );
+    }
+
+    // Now calculate our start position - this currently only works for the sector types
+    // already supported at the moment (AUDIO and Mode 2/2352).
+    let start_position =
+        mins_secs_frames[0] * 10584000 + // Minutes.
+        mins_secs_frames[1] * 176400 +   // Seconds.
+        mins_secs_frames[2] * 2352;      // Frames.
+
+    Ok(start_position)
 }
